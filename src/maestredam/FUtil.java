2e4babcd28387059695f2e9bf3850494535688d4
Raul Navarro <raulto600@gmail.com>
2023-04-26 12:35:04 +0200

    Metodoprueba implementado

--- src/maestredam/FUtil.java
+++ src/maestredam/FUtil.java
@@ -4,39 +4,40 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-
 public class FUtil {
 
 	public static void main(String[] args) {
-		
-		boolean valor = Existe();
-		
+
+		boolean valor = existe();
+
 		if (valor == false) {
 			System.out.println("No se ha encontrado el fichero");
 		} else {
-			
+
 		}
 		
-			
+		metodoprueba();
+
 	}
-	
-	public static boolean Existe() {
-		
-		
+
+	public static boolean existe() {
+
 		try (FileReader fuente = new FileReader("src/maestredam/alumnos.dam")) {
-			
+
 			return true;
-		
-		
-		}catch (FileNotFoundException fnfe) {
-			return false;	
-		
-		}catch (IOException e){
+
+		} catch (FileNotFoundException fnfe) {
+			return false;
+
+		} catch (IOException e) {
 			System.out.println("Error: " + e);
 			return false;
 		}
-				
 
+	}
+	
+	public static void metodoprueba() {
+		System.out.println("Hola");
 	}
 
 }
